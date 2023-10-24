f6254c2be8cadc81e1c43c2b5a228ed2dc29e300
Badr Choukri <bc@driversnote.com>
2023-10-24 20:49:09 +0200

    chore: update packages

---
+++  jest.config.js
@@ -0,0 +1,5 @@
+export const testEnvironment = 'node';
+export const testMatch = ['**/tests/**/*.test.js'];
+export const collectCoverage = true;
+export const collectCoverageFrom = ['src/**/*.js'];
+export const coverageReporters = ['lcov', 'text', 'html'];
